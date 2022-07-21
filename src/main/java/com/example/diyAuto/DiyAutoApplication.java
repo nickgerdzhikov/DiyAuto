09bf3c0407eecd5deffd57d4768781c1c513ea58
Nikolay (Nick) Gerdzhikov <80314646+nickgerdzhikov@users.noreply.github.com>
2022-07-21 10:27:10 -0400

    Initial commit

---
+++ src/main/java/com/example/diyAuto/DiyAutoApplication.java
@@ -0,0 +1,13 @@
+package com.example.diyAuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiyAutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiyAutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/diyAuto/DiyAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.diyAuto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiyAutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
